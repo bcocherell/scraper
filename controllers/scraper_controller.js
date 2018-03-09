2f5e97d407abe5e06333eccfe38197d87be54018
Brian Cocherell <brian.cocherell@gmail.com>
2018-03-09 10:46:25 -0500

    added catch to axios request

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -34,7 +34,8 @@ router.get("/scrape", function(req, res) {
   var newArticles = 0;
 
   // First, we grab the body of the html with request
-  axios.get("https://www.giantbomb.com/").then(function(response) {
+  axios.get("http://www.giantbomb.com/")
+  .then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
@@ -81,6 +82,10 @@ router.get("/scrape", function(req, res) {
 
     // If we were able to successfully scrape and save an Article, send a message to the client
     res.send(newArticles + ' new articles added!');
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    return res.json(err);
   });
 });
 
@@ -130,57 +135,3 @@ router.delete("/comments/:id", function(req, res) {
 });
 
 module.exports = router;
-
-// module.exports = function(app) {
-
-//   // A GET route for scraping the echojs website
-
-//   // Route for getting all Articles from the db
-//   app.get("/api/articles", function(req, res) {
-//     db.Article.find({}).sort({"date":-1})
-//       .then(function(dbArticle) {
-//         // If we were able to successfully find Articles, send them back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
-
-//   // Route for grabbing a specific Article by id, populate it with it's note
-//   app.get("/api/articles/:id", function(req, res) {
-//     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//     db.Article.findOne({ _id: req.params.id })
-//       // ..and populate all of the notes associated with it
-//       .populate("comments")
-//       .then(function(dbArticle) {
-//         // If we were able to successfully find an Article with the given id, send it back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
-
-//   //Route for saving/updating an Article's associated Note
-//   app.post("/api/articles/:id", function(req, res) {
-//     // Create a new note and pass the req.body to the entry
-//     db.Comment.create(req.body)
-//       .then(function(dbComment) {
-//         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//         return db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { comments: dbComment._id }}, { new: true });
-//       })
-//       .then(function(dbArticle) {
-//         // If we were able to successfully update an Article, send it back to the client
-//         res.json(dbArticle);
-//       })
-//       .catch(function(err) {
-//         // If an error occurred, send it to the client
-//         res.json(err);
-//       });
-//   });
-// };
